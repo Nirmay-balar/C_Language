b4a0f05033f118376de4aa711c1df75ce7eae0f0
Nirmay-balar <128052937+Nirmay-balar@users.noreply.github.com>
2023-03-27 19:10:37 +0530

    Lectue 3

---
+++ Lecture 3/CH_2_5.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+    float a=4;
+    float pi=6;
+    float r=2;
+    clrscr();
+    printf("the perimeter of circle is :%f",a*pi*r);
+    getch();
+}
---
+++ Lecture 3/CH_2_6.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+    float p=2;
+    float b=3;
+    float rt=6;
+    clrscr();
+    printf("the simple interest is:%f",p*1+rt);
+    getch();
+}
