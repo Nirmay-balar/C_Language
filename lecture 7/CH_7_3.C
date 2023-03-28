1203c95051f28f43cf9ad3dd4db1cae011bdf17f
Nirmay-balar <128052937+Nirmay-balar@users.noreply.github.com>
2023-03-28 19:02:07 +0530

    28-3-23

---
+++ lecture 7/CH_7_3.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+   int a=1,n;
+   clrscr();
+   printf("enter the value n:");
+   scanf("%d",&n);
+   while (a<=n)
+   {
+
+     printf("%d ",a);
+     ++a;
+   }
+   getch();
+}
