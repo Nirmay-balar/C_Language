138216b08eb35b90eead425ee9454778fa21f5f0
Nirmay-balar <128052937+Nirmay-balar@users.noreply.github.com>
2023-04-17 17:46:33 +0530

    Add files via upload

---
+++ task/task_3.C
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+main()
+{
+	int  i,j,r,c;
+  printf("enter the size rowes:");
+	scanf("%d",&r);
+	printf("enter the size columns:");
+	scanf("%d",&c);
+	int a[r
+	][c];
+	for(i=0;i<r;i++)
+	{
+	   for(j=0;j<c;j++)
+		    {
+    		  
+		       	scanf("%d",&a[i][j]);
+		    }
+	}
+
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+				if(i==0 && j==0 || i==0 && j==2 || i==1 && j==0 || i==1 && j==2 || i==2 && j==0 ||i==2 && j==2)
+				{
+					printf("%d ",a[i][j]);
+				}
+				else
+				{
+					printf("- ");
+				}
+	  }
+		printf("\n");
+	}
+	
+}
